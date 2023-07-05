77c23da03702cb72a5e70fa537a715b571e96c75
Cepa95 <ceprnic.josip@gmail.com>
2023-07-05 18:50:24 +0200

    deleting

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -52,6 +52,7 @@ export default {
     return {
       resources: this.storedResources,
       addResource: this.addResource,
+      deleteResource: this.removeResource,
     };
   },
 
@@ -77,6 +78,10 @@ export default {
       };
       this.storedResources.unshift(newResource);
       this.selectedTab = 'stored-resources';
+    },
+    removeResource(resId){
+      const resIndex = this.storedResources.findIndex(res => res.id == resId);
+      this.storedResources.splice(resIndex, 1)
     },
   },
 };
